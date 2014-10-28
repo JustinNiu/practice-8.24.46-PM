349ef84fff75f42375c563cad33476b54677bb01
MusicTech <musictech@lawn-143-215-113-87.lawn.gatech.edu>
2014-10-28 18:29:42 -0400

    Initial Commit

---
+++ practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by MusicTech on 10/28/14.
+//  Copyright (c) 2014 MusicTech. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
